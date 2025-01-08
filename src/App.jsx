991645e0095879ae230c469c066256eb2c39e809
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-08 23:17:00 +0600

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className="text-4xl text-center text-amber-600 font-semibold">Print Manzil Task</h2>
+    </>
+  )
+}
+
+export default App
